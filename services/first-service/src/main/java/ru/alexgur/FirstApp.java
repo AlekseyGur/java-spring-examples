b96751b6ec012feb0d68a5f14fa52b1e16c58276
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-08-29 09:41:53 +0300

    discovery-eureka

---
+++ services/first-service/src/main/java/ru/alexgur/FirstApp.java
@@ -0,0 +1,15 @@
+package ru.alexgur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FirstApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApp.class, args);
+    }
+
+}
