3e9bc65a1d1908455224f7cc2ac3f0dcec46d742
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-08-29 09:39:06 +0300

    cloud-config

---
+++ services/first-service/src/main/java/ru/alexgur/Controller.java
@@ -0,0 +1,25 @@
+package ru.alexgur;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping
+    public Map<String, String> getAllSettings() {
+        return ((AbstractEnvironment) env).getPropertySources()
+                .stream()
+                .filter(ps -> ps.containsProperty("custom"))
+                .flatMap(ps -> ((Map<String, Object>)ps.getSource()).entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().toString()));
+    }
+}
