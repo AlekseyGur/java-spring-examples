92faed6baa70c0bdb6a8fbaa5e78fff37beda0c8
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-09-07 16:16:09 +0300

    Add conf

--- services/first-service/src/main/java/ru/alexgur/Controller.java
+++ services/first-service/src/main/java/ru/alexgur/Controller.java
@@ -1,25 +1,17 @@
 package ru.alexgur;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.core.env.Environment;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
+@RefreshScope
 public class Controller {
-
-    @Autowired
-    private Environment env;
+//    @Value("${server.port}")
+//    private String port;
 
     @GetMapping
-    public Map<String, String> getAllSettings() {
-        return ((AbstractEnvironment) env).getPropertySources()
-                .stream()
-                .filter(ps -> ps.containsProperty("custom"))
-                .flatMap(ps -> ((Map<String, Object>)ps.getSource()).entrySet().stream())
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().toString()));
+    public String hello() {
+        return "Hello from service on port ";
     }
 }
